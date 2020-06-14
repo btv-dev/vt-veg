fe9acaa9657fd9ff83b75e826c839976a5e1b7a6
kylemit <Kylemit@gmail.com>
2020-06-13 22:22:12 -0400

    adding layout page

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+
+
+
+
+
+
+    return {
+        dir: {
+            "layouts": "_layouts"
+        },
+
+        // By default markdown files are pre-processing with liquid template engine
+        // https://www.11ty.io/docs/config/#default-template-engine-for-markdown-files
+        markdownTemplateEngine: "njk",
+    };
+};
